e9eab8d9b716c691db0584a84cee640c1e876e6f
karankmr1 <163286430+karankmr1@users.noreply.github.com>
2025-07-25 12:19:27 +0530

    make color changer

---
+++ index.js
@@ -0,0 +1,29 @@
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let input = document.querySelector('input')
+let body = document.querySelector("body")
+let span=document.querySelector('span')
+
+btn1.addEventListener("click", () => {
+    let randomColor = GetRandomColor()
+    body.style.backgroundColor = randomColor
+    span.innerText=body.style.backgroundColor
+})
+
+function changeColor(color){
+     body.style.backgroundColor=color
+}
+
+btn2.addEventListener("click",()=>{
+   let color = input.value
+   changeColor(color)
+})
+
+function GetRandomColor() {
+    let red = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+
+    let color = `rgb(${red},${green},${blue})`
+    return color
+}
